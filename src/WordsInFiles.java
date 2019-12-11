ab168b3724a756cc750b8b2b3513033d472e0d4f
John Buchanan Moore <johnbucmoore@gmail.com>
2019-12-11 17:31:42 -0500

    add addWordsFromFile method

--- src/WordsInFiles.java
+++ src/WordsInFiles.java
@@ -1,2 +1,45 @@
+import edu.duke.DirectoryResource;
+import edu.duke.FileResource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+
 public class WordsInFiles {
+    private HashMap<String, ArrayList<String>> wordMap;
+
+    public WordsInFiles() {
+        wordMap = new HashMap<>();
+    }
+
+    private void addWordsFromFile(File f) {
+        String name = f.getName();
+        FileResource fr = new FileResource(f)
+;        for (String word : fr.words()) {
+            if (! wordMap.containsKey(word)) {
+                ArrayList<String> fn = new ArrayList<>();
+                fn.add(name);
+                wordMap.put(word, fn);
+            } else {
+                if (! wordMap.get(word).contains(name)) {
+                    wordMap.get(word).add(name);
+                }
+            }
+        }
+    }
+
+    public void test(){
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            addWordsFromFile(f);
+        }
+        for (String key : wordMap.keySet()) {
+            System.out.println(key+" appears in: "+wordMap.get(key));
+        }
+    }
+
+    public static void main(String[] args) {
+        WordsInFiles words = new WordsInFiles();
+        words.test();
+    }
 }
